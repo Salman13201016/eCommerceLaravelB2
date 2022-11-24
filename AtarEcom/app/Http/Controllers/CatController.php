6ab375f5cfb06fbe42400de82ad47cb7ab1174f7
Salman13201016 <salmanmdsultan@gmail.com>
2022-11-24 17:28:22 +0600

    First Commit For this development

---
+++ AtarEcom/app/Http/Controllers/CatController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatController extends Controller
+{
+    //
+    function show(){
+        // echo "Cat Show";
+        return view('admin.cat');
+    }
+
+    function insert(Request $req){
+        $cat_name = $req->cat_name;
+        
+        return view('admin.cat',compact('cat_name'));
+    }
+}
